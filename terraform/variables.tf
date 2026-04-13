6a6bae445fde604024714692662185c9b7e5ca51
Vishnu Magapu <120594152+MagapuVishnu@users.noreply.github.com>
2026-04-13 11:44:35 +0200

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "azure_resource_group" {}
+variable "azure_location" {}
+
+variable "my_vnet" {}
+variable "vnet_address_space" {}
+
+variable "my_subnet" {}
+variable "subnet_address_prefix" {}
+
+variable "my_nsg" {}
+
+variable "my_public_ip" {}
+variable "my_nic" {}
+
+variable "my_linux_vm" {}
+variable "vm_size" {}
+
+variable "admin_username" {}
+variable "admin_password" {}
+
+variable "image_publisher" {}
+variable "image_offer" {}
+variable "image_sku" {}
+variable "image_version" {}
