6aa81274abfee5afe5c1ad6c7a23a23daf9aa66b
Vishnu Magapu <120594152+MagapuVishnu@users.noreply.github.com>
2026-04-13 11:45:39 +0200

    Add outputs for VM public IP, name, and resource group

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+output "vm_public_ip" {
+  value = azurerm_public_ip.publicip.ip_address
+}
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.linuxvm.name
+}
+
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
